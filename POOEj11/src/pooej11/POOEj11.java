297af46ca33e07a0db04367a61e47d5f1e35769e
Gaston Fernandez <gaston2303@gmail.com>
2022-07-19 01:42:52 -0300

    Ejercicios POO 11 y 12

--- POOEj11/src/pooej11/POOEj11.java
+++ POOEj11/src/pooej11/POOEj11.java
@@ -5,17 +5,60 @@
  */
 package pooej11;
 
+import java.util.Date;
+import java.util.Scanner;
+
 /**
  *
  * @author Gaston
  */
+/*
+Pongamos de lado un momento el concepto de POO, ahora vamos a trabajar solo con la
+clase Date. En este ejercicio deberemos instanciar en el main, una fecha usando la clase
+Date, para esto vamos a tener que crear 3 variables, dia, mes y anio que se le pedirán al
+usuario para poner el constructor del objeto Date. Una vez creada la fecha de tipo Date,
+deberemos mostrarla y mostrar cuantos años hay entre esa fecha y la fecha actual, que se
+puede conseguir instanciando un objeto Date con constructor vacío.
+Ejemplo fecha: Date fecha = new Date(anio, mes, dia);
+Ejemplo fecha actual: Date fechaActual = new Date();
+*/
 public class POOEj11 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Scanner leer = new Scanner(System.in);
+                       
+        System.out.println("Introducir una fecha");
+        System.out.print("Día: ");
+        int dia = leer.nextInt();
+        System.out.print("Mes: ");
+        int mes = leer.nextInt();
+        mes -= 1;
+        System.out.print("Año: ");
+        int anio = leer.nextInt();
+        anio -= 1900;
+        
+        Date fecha = new Date(anio,mes,dia);
+        
+        //fecha.setDate(dia);
+        //fecha.setMonth(mes);
+        //fecha.setYear(anio);
+        
+        Date fechaActual = new Date(); 
+        
+        System.out.println("Fecha ingresada: ");
+        //System.out.println(fecha.getDate()+"/"+fecha.getMonth()+"/"+fecha.getYear());
+        System.out.println(fecha.toString());
+        
+        //System.out.println(fechaActual.getDate()+"/"+fechaActual.getMonth()+"/"+fechaActual.getYear());
+        System.out.println("Fecha actual:");
+        System.out.println(fechaActual.toString());
+        System.out.println("");
+              
+        System.out.println("La Diferencia entre la fecha ingresada y la fecha es de "+(fechaActual.getYear()-fecha.getYear())+" año/s");
     }
-    
+
 }
---
+++ POOEj12/src/Servicio/PersonaServicio.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servicio;
+
+import Entidad.Persona;
+import java.util.Date;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Gaston
+ */
+/*
+Implemente la clase Persona. Una persona tiene un nombre y una fecha de nacimiento
+(Tipo Date), constructor vacío, constructor parametrizado, get y set. Y los siguientes
+métodos:
+• Agregar un método de creación del objeto que respete la siguiente firma:
+crearPersona(). Este método rellena el objeto mediante un Scanner y le pregunta al
+usuario el nombre y la fecha de nacimiento de la persona a crear, recordemos que la
+fecha de nacimiento debe guardarse en un Date y los guarda en el objeto.
+• Escribir un método calcularEdad() a partir de la fecha de nacimiento ingresada. Tener
+en cuenta que para conocer la edad de la persona también se debe conocer la fecha
+actual.
+• Agregar a la clase el método menorQue(int edad) que recibe como parámetro otra
+edad y retorna true en caso de que el receptor tenga menor edad que la persona que
+se recibe como parámetro, o false en caso contrario.
+• Metodo mostrarPersona(): este método muestra la persona creada en el método
+anterior.
+*/
+public class PersonaServicio {
+    
+    public Persona crearPersona() {
+        Scanner leer = new Scanner(System.in);
+        
+        System.out.println("Introducir datos");
+        System.out.print("Nombre: ");
+        String nombre = leer.nextLine();
+        System.out.println("Introducir echa de nacimiento");
+        System.out.print("Día: ");
+        int dia = leer.nextInt();
+        System.out.print("Mes: ");
+        int mes = leer.nextInt();
+        mes -= 1;
+        System.out.print("Año: ");
+        int anio = leer.nextInt();
+        anio -= 1900;
+        
+        Date fechaDeNacimiento = new Date();
+        
+        fechaDeNacimiento.setDate(dia);
+        fechaDeNacimiento.setMonth(mes);
+        fechaDeNacimiento.setYear(anio);
+        
+        return new Persona(nombre, fechaDeNacimiento);
+    }
+    
+    public int calcularEdad(Persona p) {
+        
+        Date fechaActual = new Date();
+        int dia = p.getFechaDeNacimiento().getDate();
+        int mes = p.getFechaDeNacimiento().getMonth();
+        int anio = p.getFechaDeNacimiento().getYear();
+        int edad;
+        
+         edad = fechaActual.getYear() - anio;
+        
+        if (mes >= fechaActual.getMonth() && dia > fechaActual.getDate()) {
+            edad -= 1;
+        }
+        
+        return edad;
+    }
+    
+    public boolean menorQue(Persona p) {
+        Scanner leer = new Scanner(System.in);
+        
+        int edad = calcularEdad(p);
+        
+        System.out.println("Introducir una edad: ");
+        int edadNueva = leer.nextInt();
+
+        boolean menor = edad < edadNueva;
+        
+        return menor;
+    }
+    
+    public void mostrarPersona(Persona p) {
+        System.out.println("----------------------------------------------------------------------------------------");
+        System.out.println(p.toString());
+        System.out.println("----------------------------------------------------------------------------------------");
+    }
+    
+}
